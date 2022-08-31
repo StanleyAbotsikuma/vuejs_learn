ee11c5db699aa1cd13e088e8a64cadb17201390e
StanleyAbotsikuma <abotsistanley0@gmail.com>
2022-08-31 09:26:47 +0000

    lecture1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Method Properties</h1>
+  <p>Variable {{var1}}</p>
+  <p>Variable {{var2}}</p>
+  <input type="number" name="" v-model="pass">
+  <p>{{var1}} * {{var2}} * {{pass}} = {{functionMethon(pass)}}</p>
+  <input type="number" name="" v-model="principal">
+  <input type="number" name="" v-model="rate">
+  <input type="number" name="" v-model="time">
+
+  <p> principal {{principal}} * rate {{rate}} *  time {{time}} /100= {{simpleInterest}}</p>
+
+  
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+ // i mport HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+   return{
+    var1:10,
+    var2:20,
+    pass:"",
+    principal:"",
+rate:"",
+time:"",
+   }
+  },
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+methods:
+{
+  functionMethon(pass)
+  {
+    return this.var1* this.var2 *pass
   }
+},computed:
+{
+ simpleInterest()
+  {
+    return this.principal* this.rate *this.time/100;
+  } 
+}
 }
 </script>
 
